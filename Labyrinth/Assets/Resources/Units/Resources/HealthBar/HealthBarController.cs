392873078d29ad19e77f87eb89bd0ebc103203aa
dmitry-petrichenko <dima.petrichenko@gmail.com>
2019-01-18 23:58:05 +0200

    update health bar

---
+++ Labyrinth/Assets/Resources/Units/Resources/HealthBar/HealthBarController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Image HealthBar;
+
+    public void Set(float value)
+    {
+        HealthBar.fillAmount = value;
+    }
+}
--- Labyrinth/Assets/Scripts/Units/OneUnit/Base/GameObject/Health/OneUnitHealthController.cs
+++ Labyrinth/Assets/Scripts/Units/OneUnit/Base/GameObject/Health/OneUnitHealthController.cs
@@ -7,12 +7,16 @@ namespace Units.OneUnit.Base.GameObject.Health
     {
         private UnityEngine.GameObject _unit;
         private LookAtCameraController _lookAtCameraController;
+        private HealthBarController _healthBarController;
         
         public OneUnitHealthController(IUnitSettings unitSettings, Camera camera)
         {
             _unit = unitSettings.GraphicObject;
             _lookAtCameraController = _unit.GetComponentInChildren(typeof(LookAtCameraController)) as LookAtCameraController;
             _lookAtCameraController.main_camera = camera;
+            
+            _healthBarController = _unit.GetComponentInChildren(typeof(HealthBarController)) as HealthBarController;
+            _healthBarController.Set(0.7f);
         }
     }
 }
