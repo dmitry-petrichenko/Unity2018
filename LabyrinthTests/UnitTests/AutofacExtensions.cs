4b27ae3e703cc31fd32fe8c2f5d8156b5351dbff
dmitry-petrichenko <dima.petrichenko@gmail.com>
2019-02-17 23:18:51 +0200

    refactoring

--- LabyrinthTests/UnitTests/AutofacExtensions.cs
+++ LabyrinthTests/UnitTests/AutofacExtensions.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Autofac;
 using Autofac.Builder;
+using Autofac.Core;
 using Tree;
 
 namespace Scripts.Extensions2
@@ -18,10 +20,58 @@ namespace Scripts.Extensions2
                scopeTree.Scope.ComponentRegistry.Register(RegistrationBuilder.ForType<TImplementer>().CreateRegistration());
 
                var disposable = scopeTree.Scope.Resolve<TImplementer>() as MyDisposable;
-               disposable.Disposed += () => scopeTree.Dispose();
+               disposable.Disposed += ()=> scopeTree.Dispose();
                return (TImplementer)disposable;
            });
            return registrationBuilder;
        }
+    }
+
+    public class ScopeTree : IScopeTree
+    {
+        private List<ScopeTree> _children = new List<ScopeTree>();
+        private ILifetimeScope _scope;
+        private bool _isDisposed;
+
+        public ScopeTree(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        public IScopeTree CreateChild(Action<ContainerBuilder> configurationAction = null)
+        {
+            var scope = configurationAction != null
+                ? _scope.BeginLifetimeScope(configurationAction)
+                : _scope.BeginLifetimeScope();
+            var child = new ScopeTree(scope);
+            _children.Add(child);
+
+            var updater = new ContainerBuilder();
+            updater.RegisterInstance(child).As<IScopeTree>();
+            updater.Update(child.Scope.ComponentRegistry);
+
+            return child;
+        }
+
+        public void Dispose()
+        {
+            if(_isDisposed)
+                return;
+
+            _children.ForEach(c=>c.Dispose());
+            _children = null;
+            _isDisposed = true;
+            _scope.Dispose();
+            _scope = null;
+        }
+
+        public ILifetimeScope Scope => _scope;
+    }
+
+    public interface IScopeTree
+    {
+        IScopeTree CreateChild(Action<ContainerBuilder> configurationAction = null);
+        void Dispose();
+        ILifetimeScope Scope { get; }
     }
 }
