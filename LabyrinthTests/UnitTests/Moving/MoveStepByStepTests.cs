58a07b16b9f9eed36d474e4dc73fad3034cd72c6
dmitry-petrichenko <dima.petrichenko@gmail.com>
2019-03-03 19:55:49 +0200

    add tests

--- LabyrinthTests/UnitTests/Moving/MoveStepByStepTests.cs
+++ LabyrinthTests/UnitTests/Moving/MoveStepByStepTests.cs
@@ -25,7 +25,7 @@ namespace Tests.Scripts.Units.Moving
         }
 
         [Theory]
-        [ClassData(typeof(MoveStepByStepTestData))]
+        [ClassData(typeof(MoveStepByStepTestData1))]
         //tested method]_[expected input]_[expected behavior]
         public void MoveTo_AnyPath_ShouldUpdateOccupationMap(List<IntVector2> path, int expectedVacantCalls, int expectedOccupiedCalls)
         {
@@ -53,19 +53,15 @@ namespace Tests.Scripts.Units.Moving
         }
         
         [Theory]
-        [ClassData(typeof(MoveStepByStepTestData))]
-        //tested method]_[expected input]_[expected behavior]
+        [ClassData(typeof(MoveStepByStepTestData1))]
         public void MoveTo_AnyPath_ShouldRiseMovePathComplete(List<IntVector2> path, int i1, int i2)
         {
             // Arrange
             bool movePathCompleteCalled = false;
-            IState state = Mock.Create<IState>();
             Mock.Arrange(() => _unitsTable.IsVacantPosition(Arg.IsAny<IntVector2>()))
                 .Returns(true);
-            Mock.Arrange(() => state.RaiseMovePathComplete())
+            Mock.Arrange(() => _stateController.CurrentState.RaiseMovePathComplete())
                 .DoInstead(() => { movePathCompleteCalled = true; });
-            Mock.Arrange(() => _stateController.CurrentState)
-                .Returns(state);
             
             _moveStepByStepController = new MoveStepByStepController(
                 _stateController, 
@@ -77,6 +73,65 @@ namespace Tests.Scripts.Units.Moving
             
             // Assert
             Assert.True(movePathCompleteCalled);
+        }
+        
+        [Theory]
+        [ClassData(typeof(MoveStepByStepTestData2))]
+        public void MoveTo_AnyPath_ShouldRiseNextTileOccupied(List<IntVector2> path, IntVector2 occupiedPosition)
+        {
+            // Arrange
+            bool nextTileOccupiedCalled = false;
+            Mock.Arrange(() => _unitsTable.IsVacantPosition(Arg.IsAny<IntVector2>()))
+                .Returns(true);
+            Mock.Arrange(() => _unitsTable.IsVacantPosition(occupiedPosition))
+                .Returns(false);
+            Mock.Arrange(() => _stateController.CurrentState.RaiseNextTileOccupied(Arg.IsAny<IntVector2>()))
+                .DoInstead(() => { nextTileOccupiedCalled = true; });
+            
+            _moveStepByStepController = new MoveStepByStepController(
+                _stateController, 
+                _unitsTable, 
+                _unitGameObjectController);
+
+            // Act
+            _moveStepByStepController.MoveTo(path);
+            
+            // Assert
+            Assert.True(nextTileOccupiedCalled);
+        }
+        
+        [Theory]
+        [InlineData(5)]
+        [InlineData(7)]
+        [InlineData(12)]
+        public void Cancel_ShouldNotRiseMovePathComplete(int moveCompleteCalledCount)
+        {
+            // Arrange
+            List<IntVector2> path = new List<IntVector2>();
+            path.Add(new IntVector2(0, 0));
+            path.Add(new IntVector2(1, 1));
+
+            int actualMoveCompleteCalledCount = 0;
+            Mock.Arrange(() => _unitsTable.IsVacantPosition(Arg.IsAny<IntVector2>()))
+                .Returns(true);
+            Mock.Arrange(() => _stateController.CurrentState.RaiseMovePathComplete())
+                .DoInstead(() => { actualMoveCompleteCalledCount++; });
+            
+            _moveStepByStepController = new MoveStepByStepController(
+                _stateController, 
+                _unitsTable, 
+                _unitGameObjectController);
+
+            // Act
+            _moveStepByStepController.MoveTo(path);
+            _moveStepByStepController.Cancel();
+            for (int i = 0; i < moveCompleteCalledCount; i++)
+            {
+                _unitGameObjectController.MoveTo(new IntVector2(0, 0));
+            }
+            
+            // Assert
+            Assert.Equal(1, actualMoveCompleteCalledCount);
         }
     }
 
