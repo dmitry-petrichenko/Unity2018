0ffed56233c5fe22260b1a1f684409a472b1b3b8
dmitry-petrichenko <dima.petrichenko@gmail.com>
2019-03-19 01:11:00 +0200

    update

--- Labyrinth/Assets/Scripts/Units/OneUnit/StatesControllers/Hostile/FreePointToGoResolver.cs
+++ Labyrinth/Assets/Scripts/Units/OneUnit/StatesControllers/Hostile/FreePointToGoResolver.cs
@@ -1,7 +1,25 @@
+using Scripts;
+using Units.OneUnit.Info;
+using Units.OneUnit.StatesControllers.Base;
+
 namespace Units.OneUnit.StatesControllers.Hostile
 {
-    public class FreePointToGoResolver
+    public class FreePointToGoResolver : IFreePointToGoResolver
     {
+        private readonly IBaseActionController _baseActionController;
+        private readonly IUnitInfoExternal _unitInfo;
         
+        public FreePointToGoResolver(
+            IBaseActionController baseActionController,
+            IUnitInfoExternal unitInfo)
+        {
+            _baseActionController = baseActionController;
+            _unitInfo = unitInfo;
+        }
+
+        public IntVector2 GetFreePoint()
+        {
+            
+        }
     }
 }
